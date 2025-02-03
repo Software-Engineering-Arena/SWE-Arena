6616442e93c250d33bf7908a06fc79267a9f35f0
JIMMY ZHAO <knightyzhao@gmail.com>
2025-02-04 05:36:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -997,7 +997,7 @@ with gr.Blocks() as app:
             Users are required to agree to the following terms before using the service:
             
             - The service is a **research preview**. It only provides limited safety measures and may generate offensive content.
-            - It must not be used for any illegal, harmful, violent, racist, or sexual purposes.
+            - It must not be used for any **illegal, harmful, violent, racist, or sexual** purposes.
             - Please **do not upload any private information**.
             - The service collects user dialogue data, including both text and images, and reserves the right to distribute it under a **Creative Commons Attribution (CC-BY)** or a similar license.
             """
