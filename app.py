541c881a92906b1af0f7f19a20cb7a4bf6b89d8f
JIMMY ZHAO <knightyzhao@gmail.com>
2025-02-14 21:51:02 +0800

    refine this

--- app.py
+++ app.py
@@ -908,6 +908,7 @@ with gr.Blocks() as app:
         )
 
         def submit_feedback(vote, models_state, conversation_state):
+            global leaderboard_data
             # Map vote to actual model names
             match vote:
                 case "Model A":
